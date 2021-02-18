15d6f4ace86c17a1d886317ee54540b8eec62b5f
danieljaimes <danieljaimes@192.168.0.8>
2021-02-17 22:18:02 -0600

    Variables

---
+++ src/variableslp1/VariablesLP1.java
@@ -0,0 +1,32 @@
+/*
+ * Lenguajes de programación I
+ * Facultad de Ingeniería
+ * Universidad Autónoma de Campeche
+ */
+package variableslp1;
+
+/**
+ *
+ * @author danieljaimes
+ */
+public class VariablesLP1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // Ejemplo de variables
+        
+        int estaEsUnaVariableEntera = 0;
+        System.out.println("estaEsUnaVariableEntera = " + estaEsUnaVariableEntera);
+        
+        estaEsUnaVariableEntera = 12;
+        System.out.println("estaEsUnaVariableEntera = " + estaEsUnaVariableEntera);
+        
+        estaEsUnaVariableEntera = 24;
+        System.out.println("estaEsUnaVariableEntera = " + estaEsUnaVariableEntera);
+        
+    }
+    
+}
